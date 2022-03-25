2200cc1d7a7a1540d1e8b8c38c6a726850c06fdd
Allen C <allenc@Zipcoders-MacBook-Pro.local>
2022-03-25 15:47:27 -0400

    Finished exercises 1 and 2. Lab completed.

---
+++ tree.py
@@ -0,0 +1,26 @@
+import os
+import logging
+from file_reader import process_file
+
+# abs path : /Users/allenc/PyCharmProjects/Python8Ex/dir1
+
+print(os.path.abspath('dir1'))
+
+
+def write_path_file():
+    file_path = os.path.abspath('dir1/file1.txt')
+
+    try:
+        if os.path.exists(file_path):
+            print('The file already exists')
+        else:
+            with open(file_path, 'w') as f:
+                f.write(f'{file_path}')
+                print("You have created a file.")
+                f.close() if f else logging.warning("No file resource available to close.")
+    except OSError as e:
+        print("Error opening the file. Please ensure the file exists and has appropriate permissions.")
+        logging.error(e)
+
+
+write_path_file()
